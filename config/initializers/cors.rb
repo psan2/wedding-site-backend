05815e048376f1e5c9f10785d74b6ad76ffd9ccc
Pat Santucci <patrick.santucci@q-free.com>
2020-10-29 17:03:34 -0700

    Adjusted origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://www.mantucciwedding.com'
+    origins 'http://mantucciwedding.com'
 
     resource '*',
       headers: :any,
